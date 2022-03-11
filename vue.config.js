49d5e36659eb46ac752f45ee181b8c97febc109b
will-wyx <wyx_msn@hotmail.com>
2022-03-11 17:03:48 +0800

    build(*): 配置打包

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+// const { resolve } = require("@babel/core/lib/vendor/import-meta-resolve");
 module.exports = defineConfig({
-  pages: {
-    index: {
-      entry: 'examples/main.js'
-    }
-  },
-  transpileDependencies: true
+    pages: {
+        index: {
+            entry: 'examples/main.js'
+        }
+    },
+    // chainWebpack: config => {
+    //     config.module
+    //         .rule('js')
+    //         .include.add(resolve(__dirname, 'packages'))
+    //         .end()
+    //         .use('babel')
+    //         .loader('babel-loader')
+    //         .tap(options => {
+    //             return options
+    //         })
+    // },
+    transpileDependencies: true
 })
