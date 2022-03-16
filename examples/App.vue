22262b0aca136f86a90bc851a87f6900f7e4bf97
will-wyx <wyx_msn@hotmail.com>
2022-03-16 12:24:26 +0800

    feat(button): 设定按钮交互背景色

--- examples/App.vue
+++ examples/App.vue
@@ -1,39 +1,21 @@
 <template>
   <div id="app">
-    <div>
-      <label for="loading">
-        <input id="loading" type="checkbox" v-model="loading"> loading
-      </label>
-    </div>
-    <wa-button type="danger" :loading="loading" @click="handleBtnClick">{{ msg }}</wa-button>
+    <button-demo/>
   </div>
 </template>
 
 <script>
 
+import ButtonDemo from "./ButtonDemo";
 export default {
   name: 'App',
-  data() {
-    return {
-      msg: 'Hello World',
-      loading: false
-    }
-  },
-  methods: {
-    handleBtnClick() {
-      console.log(this.msg)
-    }
-  },
+  components: { ButtonDemo }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 900px;
+  margin: 2rem auto;
 }
 </style>
---
+++ examples/ButtonDemo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="button-demo">
+    <div class="button-demo__options">
+      <select v-model="type">
+        <option value="default">default</option>
+        <option value="info">info</option>
+        <option value="primary">primary</option>
+        <option value="success">success</option>
+        <option value="warning">warning</option>
+        <option value="danger">danger</option>
+      </select>
+      <label for="loading">
+        <input id="loading" type="checkbox" v-model="loading"> loading
+      </label>
+    </div>
+    <div class="button-demo__private">
+      <wa-button :type="type" :loading="loading" @click="handleBtnClick">{{ msg }}</wa-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonDemo",
+  data() {
+    return {
+      msg: 'Hello World',
+      loading: false,
+      type: 'default'
+    }
+  },
+  methods: {
+    handleBtnClick() {
+      console.log(this.msg)
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.button-demo {
+  &__options {
+    margin: 1rem 0;
+  }
+}
+</style>
